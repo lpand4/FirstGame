ec40914d0a2d417e2a95befccfaaf82df2c2af63
Vladislav1 <vlad-pugovkin2@yandex.ru>
2022-10-12 21:48:32 +0300

    Написал мини игру "Угадай число"

---
+++ TestGame/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Мини игра - Угадай число.");
+Console.WriteLine("Следует угадать число, заданное рандомным образом, за наименьшее количество попыток.");
+Console.WriteLine("Итак, начнем! Введите число:");
+int numberA = new Random().Next(0,999);
+Console.WriteLine(numberA);
+int count = 0;
+int result = 0;
+string userString = Console.ReadLine();
+int numberB = Convert.ToInt32(userString);
+while(result<1)
+{
+    if( numberB == numberA)
+    {
+        Console.WriteLine("Ура! Вы угадали число!");
+        count++;
+        result++;
+    }
+    else
+    {
+        if( numberB < numberA)
+        {
+            Console.WriteLine("Твое число меньше загаданного, попробуй еще раз.");
+            userString = Console.ReadLine();
+            numberB = Convert.ToInt32(userString);
+            count++;
+         }
+        else
+        {
+            Console.WriteLine("Твое число больше загаданного, попробуй еще раз.");
+            userString = Console.ReadLine();
+            numberB = Convert.ToInt32(userString);
+            count++;
+        }
+    }
+}
+Console.Write ("Количество попыток =");
+Console.WriteLine(count);
